9b737224037eef19919666e77cb90c508d8ccd9a
ppaga <pierre.paga@gmail.com>
2021-01-31 21:01:05 +0000

    Update PE1.py
    Added test/train distinction

--- PE1.py
+++ PE1.py
@@ -7,10 +7,8 @@ from collections import namedtuple
 import matplotlib.pyplot as plt
 
 def LR_vars(**kwargs):
-
     X = np.random.randn(kwargs['N'],kwargs['theta'].shape[0])
     Y = np.matmul(X,kwargs['theta'])/np.sqrt(kwargs['theta'].shape[0]) + kwargs['sigma']*randn(kwargs['N'])
-
     return X,Y
 
 LR_key = namedtuple('LR_key',['N','alpha','sigma'])
@@ -22,18 +20,20 @@ def run_LR_simulation(N, sigma, alphas):
 
     key = LR_key(N,alpha,sigma)
 
-    X,Y = LR_vars(N=N, theta = theta, sigma = sigma)
+    X_train,Y_train = LR_vars(N=N, theta = theta, sigma = sigma)
+    X_test,Y_test = LR_vars(N=N, theta = theta, sigma = sigma)
+    
     model = LinearRegression().fit(X,Y)
-    theta_tilde = model.coef_
-    Y_tilde = model.predict(X)
-    experiments[key] = theta_tilde, theta, Y_tilde, Y
+    theta_tilde, Y_train_tilde, Y_test_tilde = model.coef_, model.predict(X_train), model.predict(X_test)
+    experiments[key] = theta_tilde, theta, Y_train_tilde, Y_train, Y_test_tilde, Y_test
   return experiments
   
 
 def plot_results(experiments):
-  fig, axes = plt.subplots(1,2,figsize = (12,20))
+  fig, axes = plt.subplots(1,3,figsize = (18,6))
   for key,val in experiments.items():
-    theta_tilde, theta, Y_tilde, Y = val
-    axes[0].scatter(Y, Y_tilde, s = 5)
+     theta_tilde, theta, Y_train_tilde, Y_train, Y_test_tilde, Y_test = val
+    axes[0].scatter(Y_train, Y_train_tilde, s = 5)
+    axes[0].scatter(Y_test, Y_test_tilde, s = 5)
     axes[1].scatter(theta, theta_tilde, s = 5)
   plt.legend([np.round(key.alpha, decimals=3) for key in experiments.keys()])
